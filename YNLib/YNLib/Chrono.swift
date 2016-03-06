e63bff6a4fc2709ff57fc986ebdf933485d3a142
Rain Qian <stoprain@Rains-MacBook-Pro.local>
2016-03-06 22:10:49 +0800

    cf time interval

--- YNLib/YNLib/Chrono.swift
+++ YNLib/YNLib/Chrono.swift
@@ -10,18 +10,18 @@ import UIKit
 
 public class Chrono {
     
-    private(set) static var ticks = [Int: NSTimeInterval]()
+    private(set) static var ticks = [Int: CFTimeInterval]()
     
     public static func start(hash: Int) {
         objc_sync_enter(ticks); defer { objc_sync_exit(ticks) }
-        ticks[hash] = NSDate().timeIntervalSince1970
+        ticks[hash] = CACurrentMediaTime()
     }
     
-    public static func stop(hash: Int, handler: (elapse: NSTimeInterval) -> Void) {
+    public static func stop(hash: Int, handler: (elapse: CFTimeInterval) -> Void) {
         objc_sync_enter(ticks); defer { objc_sync_exit(ticks) }
-        var elapse: NSTimeInterval = 0
+        var elapse: CFTimeInterval = 0
         if let t = ticks[hash] {
-            elapse = NSDate().timeIntervalSince1970 - t
+            elapse = CACurrentMediaTime() - t
             ticks.removeValueForKey(hash)
         }
         handler(elapse: elapse)
