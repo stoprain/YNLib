8822608c61b60e8c7e1f98377df1e8887d6c5e1a
Rain Qian <stoprain@Rains-MacBook-Pro.local>
2016-03-10 16:55:46 +0800

    request helper and image meta

---
+++ YNLib/YNLib/NSURLRequestHelper.swift
@@ -0,0 +1,54 @@
+//
+//  NSURLRequestHelper.swift
+//  YNLib
+//
+//  Created by stoprain on 3/10/16.
+//  Copyright © 2016 yunio. All rights reserved.
+//
+
+import UIKit
+
+public extension NSURLRequest {
+    
+    override var description : String {
+        var s = ""
+        if let url = self.URL?.absoluteString {
+            s += "curl -i '\(url)' -X \(self.HTTPMethod!)"
+        }
+        if self.allHTTPHeaderFields?.count > 0 {
+            for (key, value) in self.allHTTPHeaderFields! {
+                s += " -H '\(key)':'\(value)'"
+            }
+        }
+        if let d = self.HTTPBody {
+            if let t = NSString(data: d, encoding: NSUTF8StringEncoding) {
+                s += " -d '\(t)'"
+            }
+        }
+        return s
+    }
+    
+    static func createRequest(server: String, token: String?, tokenType: String?,
+        resource: String, method: String, body: AnyObject? = nil) -> NSMutableURLRequest {
+        let characterSet = NSMutableCharacterSet()
+        characterSet.formUnionWithCharacterSet(.URLQueryAllowedCharacterSet())
+        characterSet.removeCharactersInString("+")
+        let s = resource.stringByAddingPercentEncodingWithAllowedCharacters(characterSet)!
+        let request = NSMutableURLRequest(URL: NSURL(string: server+s)!)
+        request.HTTPMethod = method
+        request.timeoutInterval = 30
+        if let t = token, _ = tokenType {
+            request.setValue("Bearer \(t)", forHTTPHeaderField: "Authorization")
+        }
+        if let b = body {
+            do {
+                request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(b, options: NSJSONWritingOptions.PrettyPrinted)
+            } catch {
+                
+                print("Failed to build requeset \(resource)")
+            }
+        }
+        return request
+    }
+    
+}
---
+++ YNLib/YNLib/YNImageMeta.swift
@@ -0,0 +1,55 @@
+//
+//  YNImageMeta.swift
+//  Groubum
+//
+//  Created by stoprain on 8/22/15.
+//  Copyright (c) 2015 yunio. All rights reserved.
+//
+
+import UIKit
+
+public class YNImageMeta: NSObject {
+    
+    public var objectId: String = ""
+    public var size = CGSizeZero
+    public var type = PhotoType.Post
+    public var cornerRadius: CGFloat = 0
+    public var borderWidth: CGFloat = 0
+    public var borderColorHex = "#ffffff"
+    public var cacheKey: String = ""
+    public var path: String = ""
+    public var placeHolderKey: String = YNImagePlaceHolder.Post.imageName()
+    public var waterMark = true
+    public override var description: String {
+        return self.cacheKey
+    }
+    
+    public func update(s: String) {
+        
+        self.objectId = s
+        
+        let suffix = self.size.height.description + self.borderColorHex
+        self.cacheKey = self.objectId + suffix
+        self.path = YNImage.downloadPath.stringByAppendingString("/\(self.cacheKey)")
+        
+        if type == .Avatar {
+            self.placeHolderKey = YNImagePlaceHolder.Avatar.imageName()
+        }
+    }
+    
+    public func copyForQueue() -> YNImageMeta {
+        let meta = YNImageMeta()
+        meta.objectId = self.objectId
+        meta.size = self.size
+        meta.type = self.type
+        meta.cornerRadius = self.cornerRadius
+        meta.borderWidth = self.borderWidth
+        meta.borderColorHex = self.borderColorHex
+        meta.cacheKey = self.cacheKey
+        meta.path = self.path
+        meta.placeHolderKey = self.placeHolderKey
+        meta.waterMark = self.waterMark
+        return meta
+    }
+    
+}
