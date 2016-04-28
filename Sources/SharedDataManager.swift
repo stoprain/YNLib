73c8ae89d21d3333e56f9979b4c32db7f325e9f9
Rain Qian <stoprain@Rains-MacBook-Pro.local>
2016-04-28 16:52:57 +0800

    adjust sharedata key

--- Sources/SharedDataManager.swift
+++ Sources/SharedDataManager.swift
@@ -8,10 +8,6 @@
 
 import Objective_LevelDB
 
-public enum SharedDataKey : String {
-    case Setting = "setting_"
-}
-
 let ldb: LevelDB = LevelDB.databaseInLibraryWithName("shared.ldb") as! LevelDB
 
 public class SharedDataManager: NSObject {
@@ -24,28 +20,28 @@ public class SharedDataManager: NSObject {
         return ldb.objectForKey(key)
     }
     
-    public class func objectForPrefix(prefix: SharedDataKey, key: String) -> AnyObject? {
-        return ldb.objectForKey(prefix.rawValue+key)
+    public class func objectForPrefix(prefix: String, key: String) -> AnyObject? {
+        return ldb.objectForKey(prefix+key)
     }
     
     public class func setObject(object: AnyObject, key: String) {
         ldb.setObject(object, forKey: key)
     }
     
-    public class func setObject(object: AnyObject, prefix: SharedDataKey, key: String) {
-        ldb.setObject(object, forKey: prefix.rawValue+key)
+    public class func setObject(object: AnyObject, prefix: String, key: String) {
+        ldb.setObject(object, forKey: prefix+key)
     }
     
     public class func removeObjectForKey(key: String) {
         ldb.removeObjectForKey(key)
     }
     
-    public class func removeObjectForPrefix(prefix: SharedDataKey, key: String) {
-        ldb.removeObjectForKey(prefix.rawValue+key)
+    public class func removeObjectForPrefix(prefix: String, key: String) {
+        ldb.removeObjectForKey(prefix+key)
     }
     
-    public class func keysForPrefix(prefix: SharedDataKey, block: LevelDBKeyBlock) {
-        ldb.enumerateKeysBackward(false, startingAtKey: nil, filteredByPredicate: nil, andPrefix: prefix.rawValue, usingBlock: block)
+    public class func keysForPrefix(prefix: String, block: LevelDBKeyBlock) {
+        ldb.enumerateKeysBackward(false, startingAtKey: nil, filteredByPredicate: nil, andPrefix: prefix, usingBlock: block)
     }
     
     public class func logAllKeysAndValues() {
