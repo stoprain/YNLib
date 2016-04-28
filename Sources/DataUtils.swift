c6d8f36d1f6341dbc1937c2ba05ba27e036e1f64
Rain Qian <stoprain@Rains-MacBook-Pro.local>
2016-04-28 18:34:35 +0800

    link issue

--- Sources/DataUtils.swift
+++ Sources/DataUtils.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CommonCrypto
 
 public extension NSData {
     
--- Sources/YNLib.h
+++ Sources/YNLib.h
@@ -18,4 +18,3 @@ FOUNDATION_EXPORT const unsigned char YNLibVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <YNLib/PublicHeader.h>
 
 #import <YNLib/RunMode.h>
-#import <CommonCrypto/CommonCrypto.h>
