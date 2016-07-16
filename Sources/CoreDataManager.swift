90dd3dc337b26690b56f910790987a511eb65b3c
Rain Qian <stoprain@Rains-MacBook-Pro.local>
2016-07-16 13:56:52 +0800

    clean up

--- Sources/CoreDataManager.swift
+++ Sources/CoreDataManager.swift
@@ -18,7 +18,7 @@ public class CoreDataManager: NSObject {
     // MARK: - Core Data stack
     
     lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.heartsquare.CoreDataManager" in the application's documents Application Support directory.
+        // The directory the application uses to store the Core Data store file.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         return urls[urls.count-1]
         }()
