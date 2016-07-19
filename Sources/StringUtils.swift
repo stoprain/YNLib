a6bfb83618892cd798ce4b45d8a7f8b8d39fdce5
Rain Qian <stoprain@Rains-MacBook-Pro.local>
2016-07-19 12:54:06 +0800

    public

--- Sources/StringUtils.swift
+++ Sources/StringUtils.swift
@@ -8,19 +8,19 @@
 
 import UIKit
 
-extension String {
-    func boundingRectWithSize(size: CGSize, font: UIFont) -> CGRect {
+public extension String {
+    public func boundingRectWithSize(size: CGSize, font: UIFont) -> CGRect {
         return (self as NSString).boundingRectWithSize(size,
                                                        options: NSStringDrawingOptions.UsesLineFragmentOrigin,
                                                        attributes: [NSFontAttributeName: font],
                                                        context: nil)
     }
     
-    func boundingRectWithSize(size: CGSize, systemFontSize: CGFloat) -> CGRect {
+    public func boundingRectWithSize(size: CGSize, systemFontSize: CGFloat) -> CGRect {
         return self.boundingRectWithSize(size, font: UIFont.systemFontOfSize(systemFontSize))
     }
     
-    func boundingRectWithSize(size: CGSize, boldSystemFontOfSize: CGFloat) -> CGRect {
+    public func boundingRectWithSize(size: CGSize, boldSystemFontOfSize: CGFloat) -> CGRect {
         return self.boundingRectWithSize(size, font: UIFont.boldSystemFontOfSize(boldSystemFontOfSize))
     }
 }
--- YNLib.podspec
+++ YNLib.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "YNLib"
-  s.version      = "0.0.11"
+  s.version      = "0.0.12"
   s.summary      = "YNLib is a general project utitities."
 
   s.description  = <<-DESC
