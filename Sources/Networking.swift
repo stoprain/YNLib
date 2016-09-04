ab5a93f6cf39915478580f150e1d6382c82152c8
Rain Qian <stoprain@Rains-MacBook-Pro.local>
2016-09-04 21:46:45 +0800

    adding networking and data store

---
+++ Sources/Networking.swift
@@ -0,0 +1,29 @@
+//
+//  Networking.swift
+//  YNLib
+//
+//  Created by stoprain on 9/4/16.
+//  Copyright © 2016 yunio. All rights reserved.
+//
+
+public class Networking {
+    
+    public enum Method: String {
+        case OPTIONS = "OPTIONS"
+        case GET = "GET"
+        case HEAD = "HEAD"
+        case POST = "POST"
+        case PUT = "PUT"
+        case PATCH = "PATCH"
+        case DELETE = "DELETE"
+        case TRACE = "TRACE"
+        case CONNECT = "CONNECT"
+    }
+    
+    public typealias Request = (req: Int, request: NSMutableURLRequest)
+    
+    public func logRequest(request: Request) {
+        log.info("req \(request.req), \(request.request.description)")
+    }
+    
+}
